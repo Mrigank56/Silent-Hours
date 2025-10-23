8bfe189a3cfbf077557f0a31e56516d9627e7439
illusionw4 <illusionw4@gmail.com>
2025-10-23 23:50:40 +0530

    AGP 8.6.0, gradle version changed, libs version also

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.silenthours"
-        minSdk = 29
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
